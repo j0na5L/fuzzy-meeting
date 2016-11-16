c2fab0b8391ad0af5511656641299a11e15a3ce9
Jonas Lydmark <jonas.lydmark@gmail.com>
2016-11-16 22:40:51 +0100

    some changes

---
+++ src/main/java/se/jlydmark/fuzzy/model/GroupData.java
@@ -0,0 +1,10 @@
+package se.jlydmark.fuzzy.model;
+
+import java.util.Collection;
+
+public class GroupData{
+   
+   private Long groupId;
+   private String groupName;
+   private Collection<UserData> users;
+}
--- src/main/java/se/jlydmark/fuzzy/model/UserData.java
+++ src/main/java/se/jlydmark/fuzzy/model/UserData.java
@@ -1,6 +1,66 @@
 package se.jlydmark.fuzzy.model;
 
+import java.util.Collection;
+import java.util.HashSet;
 
-public class UserData{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
+
+@Entity
+public class UserData extends AbstractEntityData{
+   
+   @Column(unique = true, updatable = false)
+   private int userId;
+   @Column(nullable = false)
+   private String username;
+   @Column(nullable = false)
+   private String firstName;
+   @Column(nullable = false)
+   private String lastName;
+   @OneToMany
+   private Collection<MeetingData> meetings;
+   @ManyToMany
+   private Collection<GroupData> groups;
+   
+   
+   public UserData(int userId, String username, String firstName, String lastName){
+      this.userId = userId;
+      this.username = username;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.meetings = new HashSet();
+   }
+   
+   
+   public int getUserId(){
+      return this.userId;
+   }
+   
+   
+   public String getUsername(){
+      return this.username;
+   }
+   
+   
+   public String getFirstName(){
+      return this.firstName;
+   }
+   
+   
+   public String getLastName(){
+      return this.lastName;
+   }
+   
+   
+   public Collection<MeetingData> getMeetings(){
+      return this.meetings;
+   }
+   
+   
+   public Collection<GroupData> getGroups(){
+      return this.groups;
+   }
 }
